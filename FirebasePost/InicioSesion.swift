08fa399d7d40114304141e1b8119fde2e5e6b414
Kiara Itzel Laredo González <kiara_itzel@hotmail.com>
2022-02-23 15:43:45 -0600

    feat: add login initial variables and inicio interface

--- FirebasePost/InicioSesion.swift
+++ FirebasePost/InicioSesion.swift
@@ -6,13 +6,17 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class InicioSesion: UIViewController {
 
+    @IBOutlet weak var correo: UITextField!
+    @IBOutlet weak var password: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-
+    @IBAction func entrar(_ sender: UIButton) {
+    }
+    
 }
