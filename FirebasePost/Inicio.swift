38f6acbae81f1f82f6aa638771049a7fb01a3dfb
Kiara Itzel Laredo González <kiara_itzel@hotmail.com>
2022-02-24 23:42:47 -0600

    style: add a gray layer behind the overlay view

--- FirebasePost/Inicio.swift
+++ FirebasePost/Inicio.swift
@@ -12,6 +12,8 @@ class Inicio: UIViewController {
     @IBOutlet var vistaPost: UIView!
     @IBOutlet weak var texto: UITextView!
     
+    let customView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         vistaPost.layer.cornerRadius = 10
@@ -19,12 +21,23 @@ class Inicio: UIViewController {
     
     @IBAction func guardarPost(_ sender: UIBarButtonItem) {
         
+        
     }
     @IBAction func agregarPost(_ sender: UIBarButtonItem) {
+        
+        let w = self.view.frame.width
+        let h = self.view.frame.height
+        
+        customView.frame = CGRect.init(x: 0, y: 0, width: w, height: h)
+        customView.backgroundColor = UIColor.gray.withAlphaComponent(0.5)
+        customView.center = self.view.center
+        self.view.addSubview(customView)
+        
         vistaPost.center = self.view.center
         self.view.addSubview(vistaPost)
     }
     @IBAction func cancelarPost(_ sender: UIBarButtonItem) {
+        customView.removeFromSuperview()
         vistaPost.removeFromSuperview()
     }
     
