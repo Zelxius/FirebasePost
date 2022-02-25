6accf8c7b4a0849b0bb51e01c40bdec04159cc8b
Kiara Itzel Laredo González <kiara_itzel@hotmail.com>
2022-02-24 23:12:46 -0600

    feat: create agregarPost interface

--- FirebasePost/Inicio.swift
+++ FirebasePost/Inicio.swift
@@ -9,12 +9,25 @@ import UIKit
 import FirebaseAuth
 class Inicio: UIViewController {
 
+    @IBOutlet var vistaPost: UIView!
+    @IBOutlet weak var texto: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        vistaPost.layer.cornerRadius = 10
     }
     
-
+    @IBAction func guardarPost(_ sender: UIBarButtonItem) {
+        
+    }
+    @IBAction func agregarPost(_ sender: UIBarButtonItem) {
+        vistaPost.center = self.view.center
+        self.view.addSubview(vistaPost)
+    }
+    @IBAction func cancelarPost(_ sender: UIBarButtonItem) {
+    }
+    
+    
     @IBAction func salir(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Salir", message: "¿Desea salir?", preferredStyle: .alert)
         let aceptar = UIAlertAction(title: "ACEPTAR", style: .default) { (_) in
